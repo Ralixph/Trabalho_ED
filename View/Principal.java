19ead042b7bf4a83e3e20f4b07983e43bc4c6e87
ArthurSantosAlmeida <arthursa.prof@gmail.com>
2025-06-09 15:49:55 -0300

    copia pro principal

--- View/Principal.java
+++ View/Principal.java
@@ -3,14 +3,10 @@ package View;
 public class Principal {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		// TODO Auto-generated method stub
 		int comida;
-=======
-		
 		int i = 0;
 
->>>>>>> branch 'master' of https://github.com/Ralixph/Trabalho_ED.git
 	}
 
 }
